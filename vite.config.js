048d890094efb86c945185d4da5c05556de72964
orenzhang <orenzhang@ovinc.cn>
2025-12-18 21:18:16 +0800

    feat(vite): update asset file naming and add wxLogin.js

--- vite.config.js
+++ vite.config.js
@@ -2,36 +2,46 @@ import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [vue()],
-  define: {
-    'process.env': {
-      BACKEND_URL: process.env.BACKEND_URL,
-      SITE_URL: process.env.SITE_URL,
-    },
-  },
-  base: '/',
-  publicDir: 'public',
-  server: {
-    host: process.env.HOST,
-    port: 8080,
-    allowedHosts: [(new URL(process.env.SITE_URL)).host],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  preview: {},
-  css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false,
+export default defineConfig(() => {
+  const timestamp = new Date().getTime();
+  return {
+    plugins: [vue()],
+    define: {
+      'process.env': {
+        BACKEND_URL: process.env.BACKEND_URL,
+        SITE_URL: process.env.SITE_URL,
       },
     },
-  },
-  build: {
-    sourcemap: false,
-    chunkSizeWarningLimit: 2000,
-  },
+    base: '/',
+    publicDir: 'public',
+    server: {
+      host: process.env.HOST,
+      port: 8080,
+      allowedHosts: [(new URL(process.env.SITE_URL)).host],
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    preview: {},
+    css: {
+      preprocessorOptions: {
+        scss: {
+          charset: false,
+        },
+      },
+    },
+    build: {
+      sourcemap: false,
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name]-[hash]-${timestamp}.js`,
+          chunkFileNames: `assets/[name]-[hash]-${timestamp}.js`,
+          assetFileNames: `assets/[name]-[hash]-${timestamp}[extname]`,
+        },
+      },
+    },
+  };
 });
